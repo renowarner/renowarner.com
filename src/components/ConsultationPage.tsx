24818b1f97a5321a3280f8b39c74b9e216d61c77
Devlin <reno@renowarner.com>
2026-01-22 06:34:15 -0800

    Embed Calendly inline widget on Consultation page

--- src/components/ConsultationPage.tsx
+++ src/components/ConsultationPage.tsx
@@ -1,15 +1,26 @@
+import { useEffect } from 'react';
 import './ConsultationPage.css';
 
 const ConsultationPage = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="container">
       <div className="consultation-content">
-        <p>
-          Please embed your calendar link below.
-        </p>
-        <div className="calendar-placeholder">
-          {/* Embed your calendar link here */}
-        </div>
+        <div 
+          className="calendly-inline-widget" 
+          data-url="https://calendly.com/rdwarner/rwmeet?hide_event_type_details=1&hide_gdpr_banner=1" 
+          style={{ minWidth: '320px', height: '700px' }} 
+        />
       </div>
     </div>
   );
